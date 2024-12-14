9b4a4c2bc3dac942fffe59eee53ef5cba23de44f
Celal Zamanoglu <celal@nar.tech>
2024-12-14 22:00:42 +0300

    fix: iOS auto-zoom on input focus distorting other inputs' functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,23 @@
-import { Providers } from "./providers";
-import "./globals.css";
+import { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 
+import { Providers } from "./providers";
+
+import "./globals.css";
+
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "MedAssistant",
+  description: "MedAssistant is an AI-powered medical assistant",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: "cover",
+};
 
 export default function RootLayout({
   children,
